f9feba6d60a14a3ca0f8bfaca6936e4d2f6820ff
alvar <alvar@DESKTOP-EJJD748>
2021-02-14 12:50:44 +0100

    Se va a realizar el primer commit de Eclipse

---
+++ eclipse-workspace/ED04/src/Deposito/Ccuenta.java
@@ -0,0 +1,5 @@
+package Deposito;
+
+public class Ccuenta {
+
+}
---
+++ eclipse-workspace/ED04/src/Deposito/claseMain.java
@@ -0,0 +1,10 @@
+package Deposito;
+
+public class claseMain {
+
+	public static void main(String[] args) {
+		// TODO Esbozo de método generado automáticamente
+
+	}
+
+}
